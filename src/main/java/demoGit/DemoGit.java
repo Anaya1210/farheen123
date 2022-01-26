ec239f7ed036557bb3088a39b4023c0fa52c856a
HP <HP@DESKTOP-8GPV7J2>
2022-01-26 23:06:59 +0530

    user2 commit

--- src/main/java/demoGit/DemoGit.java
+++ src/main/java/demoGit/DemoGit.java
@@ -1,5 +1,17 @@
 package demoGit;
 
+import org.testng.annotations.Test;
+
 public class DemoGit {
+	
+	@Test
+	public void Demo()
+	{
+		System.out.println("hii this is user2 commit");
+	}
+
+	
+	
+	
 
 }
